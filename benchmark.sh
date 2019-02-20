5751bffe409136b2887e14f9bb98e1be43ec2852
Gamer <gamer@gamer-System-Product-Name>
2019-02-20 17:36:49 +0800

    CGPU_timing.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,94 +1,97 @@
 #!/bin/bash
 
+#declare -a mpi=("o" "x")
 declare -a device=("gpu")
+declare -a timesol=("o" "x")
 declare -a precision=("single" "double")
-#declare -a numa=("PE" "SPAN" "OVERSUBSCRIBE" "NOOVERSUBSCRIBE")
-declare -a numa=("PE")
+declare -a numa=("x")
 
 cores=16
 
-process=(2 4 8 16 32)
-threads=(32 16 8 4 2 1)
+#process=(2 4 8 16 32)
+#threads=(32 31 30 29 28 27 26 25 24  16 10 9 8 7 6)
 
-#if [ -f summary.csv ]; then
-# rm summary.csv
-#fi
-#
-#printf '%s\n' device precision process "# of threads per process" "-map-by numa:" performance | paste -sd ',' >> summary.csv
-####################### Process == 1 #################################
+if [ -f summary.csv ]; then
+ rm summary.csv
+fi
+
+#printf '%s\n' device timesol threads performance | paste -sd ',' >> summary.csv
+###################### Process == 1 #################################
+
+   for d in "${device[@]}"
+do for s in "${timesol[@]}"
+do for t in {32..1}
+do for n in "${numa[@]}"
+do
+   t0=$( printf %02d ${t} )
+
+#  if  (( "${t}" == 16 )) || (( "${t}" == 32 )); then
+        if [ -d "${d}.timsol_${s}.thread_${t0}.numa_${n}" ]; then
+          cd     ${d}.timsol_${s}.thread_${t0}.numa_${n}
+          rm -rf *
+        else
+          mkdir  ${d}.timsol_${s}.thread_${t0}.numa_${n}
+          cd     ${d}.timsol_${s}.thread_${t0}.numa_${n}
+        fi
+
+        ln -s ../bin/${d}.single.mpi_x.timesol_${s}/gamer .
+        
+        cp ../input/Input__* .
+        sed -i "s/^OMP_NTHREAD *.*/OMP_NTHREAD                   ${t}/" Input__Parameter
+        ln -s ../restart/single/RESTART .
+
+        ./gamer >& log &
+        echo "${d},timing solver_${s} , thread=${t} is running..."
+      
+        PID=$!
+        wait $PID
+#        perf=`cat Record__Performance | awk 'FNR == 3 {print $7}'`
+#        printf '%s\n' ${d} ${p} 1 ${t} - ${perf} | paste -sd ',' >> ../summary.csv
+        printf "Done!\n\n"
+        cd ../
+#  fi
+
+done
+done
+done
+done
+
+###################### Process > 1 #################################
 #
 #   for d in "${device[@]}"
 #do for p in "${precision[@]}"
+#do for n in "${process[@]}"
 #do for t in "${threads[@]}"
+#do for i in "${numa[@]}"
 #do
+#   nt=$(( n*t ))
+#   n0=$( printf %02d ${n} )
 #   t0=$( printf %02d ${t} )
+#  nt0=$( printf %02d ${nt} )
 #
-#  if  (( "${t}" == 16 )) || (( "${t}" == 32 )); then
-#        if [ -d "${d}.${p}.process_01.thread_${t0}" ]; then
-#          cd     ${d}.${p}.process_01.thread_${t0}
+#
+#  if  (( "${nt}" == 16 )) || (( "${nt}" == 32 )); then
+#        if [ -d "${d}.${p}.process_${n0}.thread_${t0}.numa_${i}" ]; then
+#          cd     ${d}.${p}.process_${n0}.thread_${t0}.numa_${i}
 #          rm -rf *
 #        else
-#          mkdir  ${d}.${p}.process_01.thread_${t0}
-#          cd     ${d}.${p}.process_01.thread_${t0}
+#          mkdir  ${d}.${p}.process_${n0}.thread_${t0}.numa_${i}
+#          cd     ${d}.${p}.process_${n0}.thread_${t0}.numa_${i}
 #        fi
 #
-#        ln -s ../bin/${d}.${p}.mpi_x/gamer .
+#        ln -s ../bin/${d}.${p}.mpi_o/gamer .
 #        
 #        cp ../input/Input__* .
 #        sed -i "s/^OMP_NTHREAD *.*/OMP_NTHREAD                   ${t}/" Input__Parameter
 #        ln -s ../restart/${p}/RESTART .
 #
-#        ./gamer >& log &
-#        echo "${d}, ${p}, process=01, thread=${t0} is running..."
-#      
-#        PID=$!
-#        wait $PID
-#        perf=`cat Record__Performance | awk 'FNR == 3 {print $7}'`
-#        printf '%s\n' ${d} ${p} 1 ${t} - ${perf} | paste -sd ',' >> ../summary.csv
-#        printf "Done!\n\n"
-#        cd ../
-#  fi
-#
-#done
-#done
-#done
-
-###################### Process > 1 #################################
-
-   for d in "${device[@]}"
-do for p in "${precision[@]}"
-do for n in "${process[@]}"
-do for t in "${threads[@]}"
-do for i in "${numa[@]}"
-do
-   nt=$(( n*t ))
-   n0=$( printf %02d ${n} )
-   t0=$( printf %02d ${t} )
-  nt0=$( printf %02d ${nt} )
-
-
-  if  (( "${nt}" == 16 )) || (( "${nt}" == 32 )); then
-        if [ -d "${d}.${p}.process_${n0}.thread_${t0}.numa_${i}" ]; then
-          cd     ${d}.${p}.process_${n0}.thread_${t0}.numa_${i}
-          rm -rf *
-        else
-          mkdir  ${d}.${p}.process_${n0}.thread_${t0}.numa_${i}
-          cd     ${d}.${p}.process_${n0}.thread_${t0}.numa_${i}
-        fi
-
-        ln -s ../bin/${d}.${p}.mpi_o/gamer .
-        
-        cp ../input/Input__* .
-        sed -i "s/^OMP_NTHREAD *.*/OMP_NTHREAD                   ${t}/" Input__Parameter
-        ln -s ../restart/${p}/RESTART .
-
-        if [ ${i} == "PE" ] && (( "$((${cores}/${n}))" >= 1 )); then
-          mpirun -np ${n} -map-by numa:pe=$((${cores}/${n})) --report-bindings  ./gamer >& log &
-          echo "${d}, ${p}, process=${n0}, thread=${t0}, numa:pe=$((${cores}/${n})) is running..."
-          PID=$!
-          wait $PID
-          perf=`cat Record__Performance | awk 'FNR == 3 {print $7}'`
-          printf '%s\n' ${d} ${p} ${n} ${t} "pe=$((${cores}/${n}))" ${perf} | paste -sd ',' >> ../summary.csv
+#        if [ ${i} == "PE" ] && (( "$((${cores}/${n}))" >= 1 )); then
+#          mpirun -np ${n} -map-by numa:pe=$((${cores}/${n})) --report-bindings  ./gamer >& log &
+#          echo "${d}, ${p}, process=${n0}, thread=${t0}, numa:pe=$((${cores}/${n})) is running..."
+#          PID=$!
+#          wait $PID
+#          perf=`cat Record__Performance | awk 'FNR == 3 {print $7}'`
+#          printf '%s\n' ${d} ${p} ${n} ${t} "pe=$((${cores}/${n}))" ${perf} | paste -sd ',' >> ../summary.csv
 #        elif [ ${i} != "PE" ]; then
 #          mpirun -np ${n} -map-by numa:${i} --report-bindings  ./gamer >& log &
 #          echo "${d}, ${p}, process=${n0}, thread=${t0}, numa:${i} is running..."
@@ -96,16 +99,16 @@ do
 #          wait $PID
 #          perf=`cat Record__Performance | awk 'FNR == 3 {print $7}'`
 #          printf '%s\n' ${d} ${p} ${n} ${t} ${i} ${perf} | paste -sd ',' >> ../summary.csv
-        fi
-      
-        printf "Done!\n\n"
-        cd ../
-  fi
-
-####################################################################
-
-done
-done
-done
-done
-done
+#        fi
+#      
+#        printf "Done!\n\n"
+#        cd ../
+#  fi
+#
+#####################################################################
+#
+#done
+#done
+#done
+#done
+#done
---
+++ perf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -f "thread_vs_perf.csv" ]; then
+ rm thread_vs_perf.csv
+fi
+
+for t in {32..1}
+do
+
+  t0=$( printf %02d ${t} )
+  
+  perf=`cat gpu.timsol_x.thread_${t0}.numa_x/Record__Performance | awk 'FNR == 3 {print $7}'`
+  
+  printf '%s\n' ${t} ${perf} | paste -sd ' ' >> thread_vs_perf.csv
+
+done
