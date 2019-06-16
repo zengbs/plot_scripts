dfd1d0c3c075dea54effd7516f3a7bb8482fc7b1
Tseng, Po-Hsun <zengbs@gmail.com>
2019-06-16 17:09:44 +0800

    add slice.py

--- slice/slice.py
+++ slice/slice.py
@@ -93,30 +93,18 @@ def _number_density(field, data):
 # load the command-line parameters
 parser = argparse.ArgumentParser( description='Plot slices for sr-hydro' )
 
-parser.add_argument
-( '-f', action='store',  required=True,  type=str,   dest='field',     help='field you want to plot' )
-parser.add_argument
-( '-p', action='store',  required=True,  type=str,   dest='cut_axis',  help='axis you want to cut' )
-parser.add_argument
-( '-sx', action='store', required=True,  type=float, dest='sart_cut',  help='start cut' )
-parser.add_argument
-( '-ex', action='store', required=True,  type=float, dest='end_cut',   help='end cut' )
-parser.add_argument
-( '-nx', action='store', required=True,  type=int,   dest='N_cut',     help='number of cuts between -sx and -ex' )
-parser.add_argument
-( '-st', action='store', required=True,  type=int,   dest='idx_start', help='first data index' )
-parser.add_argument
-( '-et', action='store', required=True,  type=int,   dest='idx_end',   help='last data index' )
-parser.add_argument
-( '-dt', action='store', required=False, type=int,   dest='didx',      help='delta data index [%(default)d]', default=1 )
-parser.add_argument
-( '-i', action='store',  required=False, type=str,   dest='prefix',    help='data path prefix [%(default)s]', default='./' )
-parser.add_argument
-( '-o', action='store',  required=True,  type=int,   dest='zoom',      help='zoom in' )
-parser.add_argument
-( '-l', action='store',  required=True,  type=bool,  dest='log',       help='log scale' )
-parser.add_argument
-( '-g', action='store',  required=True,  type=bool,  dest='grid',      help='grids' )
+parser.add_argument( '-f', action='store',  required=True,  type=str,   dest='field',     help='pressure, 4-velocity_x/y/z, Lorentz_factor' )
+parser.add_argument( '-p', action='store',  required=True,  type=str,   dest='cut_axis',  help='axis you want to cut' )
+parser.add_argument( '-sx', action='store', required=True,  type=float, dest='start_cut',  help='start cut' )
+parser.add_argument( '-ex', action='store', required=True,  type=float, dest='end_cut',   help='end cut' )
+parser.add_argument( '-nx', action='store', required=True,  type=float, dest='N_cut',     help='number of cuts between -sx and -ex' )
+parser.add_argument( '-st', action='store', required=True,  type=int,   dest='idx_start', help='first data index' )
+parser.add_argument( '-et', action='store', required=True,  type=int,   dest='idx_end',   help='last data index' )
+parser.add_argument( '-dt', action='store', required=False, type=int,   dest='didx',      help='delta data index [%(default)d]', default=1 )
+parser.add_argument( '-i', action='store',  required=False, type=str,   dest='prefix',    help='data path prefix [%(default)s]', default='./' )
+parser.add_argument( '-o', action='store',  required=True,  type=int,   dest='zoom',      help='zoom in' )
+parser.add_argument( '-l', action='store',  required=True,  type=bool,  dest='log',       help='log scale' )
+parser.add_argument( '-g', action='store',  required=True,  type=bool,  dest='grid',      help='grids' )
 
 args=parser.parse_args()
 
@@ -138,89 +126,95 @@ idx_end     = args.idx_end
 didx        = args.didx
 prefix      = args.prefix
 zoom        = args.zoom
-log         = args.l
+log         = args.log
+grid        = args.grid
 
 colormap    = 'arbre'
 
 dpi         = 150
 
-if   field is 'proper_number_density':
-        unit= '1/code_length**3'
-        function='_number_density'
-elif field is 'Lorentz_factor':
-        unit = ''
-        function='_lorentz_factor'
-elif field is 'pressure':
-        unit= 'code_mass/(code_length*code_time**2)'
-        function='_pressure_sr'
-elif field is '4-velocity_x':
-        unit= 'code_length/code_time'
-        function='_Ux_sr'
-elif field is '4-velocity_y':
-        unit= 'code_length/code_time'
-        function='_Uy_sr'
-elif field is '4-velocity_z':
-        unit= 'code_length/code_time'
-        function='_Uz_sr'
-elif field is 'enthalpy':
-        unit= ''
-        function='_enthalpy'
-elif field is 'energy_per_volume':
-        unit = 'code_mass/(code_length*code_time**2)'
-        function=''
-elif field is 'number_density':
-        unit = '1/code_length**3'
-        function='_number_density'
-elif field in ('momentum_x', 'momentum_y', 'momentum_z'):
+
+if field == 'proper_number_density':
+      unit= '1/code_length**3'
+      function=_number_density
+if field == 'Lorentz_factor':
+      unit = ''
+      function=_lorentz_factor
+if field == 'pressure':
+      unit= "code_mass/(code_length*code_time**2)"
+      function=_pressure_sr
+if field == '4-velocity_x':
+      unit= 'code_length/code_time'
+      function = _Ux_sr
+if field == '4-velocity_y':
+      unit= 'code_length/code_time'
+      function=_Uy_sr
+if field == '4-velocity_z':
+      unit= 'code_length/code_time'
+      function=_Uz_sr
+if field == 'enthalpy':
+      unit= ''
+      function=_enthalpy
+if field == 'energy_per_volume':
+      unit = 'code_mass/(code_length*code_time**2)'
+if field == 'number_density':
+      unit = '1/code_length**3'
+      function=_number_density
+if field in ('momentum_x', 'momentum_y', 'momentum_z'):
         unit = 'code_mass/(code_time*code_length**2)'
 
 yt.enable_parallelism()
 
 ts = yt.load( [ prefix+'/Data_%06d'%idx for idx in range(idx_start, idx_end+1, didx) ] )
 
+
 for ds in ts.piter():
-center = ds.domain_center
 
-x_shift = y_shift = z_shift = 0.0
+ center = ds.domain_center
 
- for shift in np.arange(start_cut, end_cut, (start_cut-end_cut)/N_cut):
+ print(start_cut, end_cut, np.fabs(start_cut-end_cut)/N_cut)
 
-   if   cut_axis is 'x'
-     x_shift = shift
-   elif cut_axis is 'y'
-     y_shift = shift 
-   elif cut_axis is 'z'
-     z_shift = shift 
+# for origin in np.arange(start_cut, end_cut, np.fabs(start_cut-end_cut)/N_cut):
+ origin = start_cut
+
+ while origin <= end_cut:
+
+   if cut_axis == 'x':
+     center[0] = origin
+   elif cut_axis == 'y':
+     center[1] = origin
+   elif cut_axis == 'z':
+     center[2] = origin
    
-   center[0] += x_shift*ds.length_unit
-   center[1] += y_shift*ds.length_unit
-   center[2] += z_shift*ds.length_unit
-
 # add new derived field
-   ds.add_field( ("gamer", field)  , function=_number_density  , sampling_type="cell", unit=unit )
+#   if  field != 'momentum_x' ||  field != 'momentum_y' ||  field != 'momentum_z' ||  field != 'energy_per_volume'
+   ds.add_field( ("gamer", field)  , function=function  , sampling_type="cell", units=unit )
 
    sz = yt.SlicePlot( ds, cut_axis, field, center=center, origin='native'  )
    sz.set_zlim( field, 'min', 'max')
    sz.set_log( field, log )
    sz.zoom(zoom)
 
-   if cut_axis is 'x':
+   if cut_axis == 'x':
      sz.set_xlabel('y (grid)')
      sz.set_ylabel('z (grid)')
-     cut_plane ='x='+ str('%3.3f'center[0])
-   elif cut_axis is 'y':
+     x='%0.3f'% center[0]
+     cut_plane ='x_'+x.zfill(8)
+   elif cut_axis == 'y':
      sz.set_xlabel('z (grid)')
      sz.set_ylabel('x (grid)')
-     cut_plane ='y='+ str('%3.3f'center[1])
-   elif cut_axis is 'z':
+     y='%0.3f'% center[1]
+     cut_plane ='y_'+y.zfill(8)
+   elif cut_axis == 'z':
      sz.set_xlabel('x (grid)')
      sz.set_ylabel('y (grid)')
-     cut_plane ='z='+ str('%3.3f'center[2])
+     z='%0.3f'% center[2]
+     cut_plane ='z_'+z.zfill(8)
 
    sz.annotate_title('slice plot (' + cut_plane + ')')
    sz.set_font({'weight':'bold', 'size':'22'})
 
-   if field is 'g'
+   if field == 'g':
      sz.annotate_velocity(factor = 16, normalize=True)
 
    sz.annotate_timestamp( time_unit='code_time', corner='upper_right', time_format='t = {time:.2f} grid$/c$', text_args={'color':'black'})
@@ -230,4 +224,6 @@ x_shift = y_shift = z_shift = 0.0
    sz.annotate_grids( periodic=grid )
 #   sz.annotate_line((70,80),(70,0), coord_system='plot')
 #   sz.save( mpl_kwargs={"dpi":dpi} )
-   sz.save( name='Data_%06d_' + str(cut_plane), mpl_kwargs={"dpi":dpi} )
+   sz.save( name='Data_%06d_' %idx_start + str(cut_plane), mpl_kwargs={"dpi":dpi} )
+
+   origin += np.fabs(start_cut-end_cut)/N_cut
