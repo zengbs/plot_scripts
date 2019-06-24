98cc38db836a8104f110a1593de3fa562931c966
Tseng, Po-Hsun <zengbs@gmail.com>
2019-06-24 11:31:24 +0800

    minor update

--- KeyInData.py
+++ KeyInData.py
@@ -4,6 +4,6 @@ ds=yt.load("RESTART")
 
 #for key in ds:
 #  print (key, ds[key])
-print (ds["Step"])
-print (ds["DumpID"])
-print (ds["EoS"])
+print ('Step:', ds["Step"])
+print ('DumpID:', ds["DumpID"])
+print ('EoS:', ds["EoS"])
