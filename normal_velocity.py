980b150eeafc1e73c2c08f63b04c4f09e690922d
Tseng, Po-Hsun <zengbs@gmail.com>
2019-07-15 23:02:15 +0800

    update

--- normal_velocity.py
+++ normal_velocity.py
@@ -1,11 +1,8 @@
-import argparse
-import sys
 import yt
 import numpy as np
-import yt.visualization.eps_writer as eps
 import derived_field as df
-import time
 from numpy import linalg as LA
+import matplotlib.pyplot as plt
 
 df.ds = yt.load('Data_000001')
 
@@ -15,12 +12,9 @@ df.ds.add_field( ("gamer", '4-velocity_x')    , function=df._4_velocity_x    , s
 df.ds.add_field( ("gamer", '4-velocity_y')    , function=df._4_velocity_y    , sampling_type="cell", units='code_length/code_time' )
 df.ds.add_field( ("gamer", '4-velocity_z')    , function=df._4_velocity_z    , sampling_type="cell", units='code_length/code_time' )
    
-# df.ds.add_gradient_fields(("gamer",field))
 df.ds.periodicity = (True, True, True)
 
 ad = df.ds.all_data()
-# cr = ad.cut_region(["obj['Lorentz_factor'] >= 10.0"])
-#surf = df.ds.surface(ad, 'Lorentz_factor', 1.0)
 
 vertice1, Ux = ad.extract_isocontours('Lorentz_factor', 10.5, sample_values='4-velocity_x')
 vertice2, Uy = ad.extract_isocontours('Lorentz_factor', 10.5, sample_values='4-velocity_y')
@@ -43,8 +37,51 @@ NormSquare = LA.norm( normal, axis=1 )**2
 
 NormalVelocity = np.transpose( [NormalDotU / NormSquare] ) * normal
 
+NormalLorentzFactor = np.sqrt( 1.0 + np.sum ( NormalVelocity**2, axis=1 ) )
 
+#########################################################
 
+UniqueVertice = np.unique(vertice1, axis=0)
+
+IdxVertice = np.zeros(vertice1.shape[0])
+
+for idx in range(vertice1.shape[0]):
+    IdxVertice[idx:idx+1] = UniqueVertice.tolist().index(vertice1[idx:idx+1,:].tolist()[0])
+
+IdxVertice.shape = (int(IdxVertice.shape[0]/3), 3)
+
+IdxVertice.astype(int)
+
+#########################################################
+
+colormap = plt.get_cmap('jet')
+
+NomalizationConst = 1.0/( np.amax(NormalLorentzFactor)-np.amin(NormalLorentzFactor) )
+
+RGB = colormap(NormalLorentzFactor*NomalizationConst)*255
+
+RGB = RGB.astype(int)
+
+RGB = np.delete(RGB, 3, 1)
+
+#########################################################
+
+print ("RGB--------------------")
+print ( RGB )
+print("ndim:{0}".format(RGB.ndim))
+print("shape:{0}".format(RGB.shape))
+print ("IdxVertice--------------------")
+print ( IdxVertice )
+print("ndim:{0}".format(IdxVertice.ndim))
+print("shape:{0}".format(IdxVertice.shape))
+print ("UniqueVertice--------------------")
+print ( UniqueVertice )
+print("ndim:{0}".format(UniqueVertice.ndim))
+print("shape:{0}".format(UniqueVertice.shape))
+print ("vertice1--------------------")
+print ( vertice1 )
+print("ndim:{0}".format(vertice1.ndim))
+print("shape:{0}".format(vertice1.shape))
 print ("vector1--------------------")
 print ( vector1 )
 print("ndim:{0}".format(vector1.ndim))
@@ -81,3 +118,9 @@ print ("NormalVelocity--------------------")
 print ( NormalVelocity )
 print("ndim:{0}".format(NormalVelocity.ndim))
 print("shape:{0}".format(NormalVelocity.shape))
+print ("NormalLorentzFactor--------------------")
+print ( NormalLorentzFactor )
+print("ndim:{0}".format(NormalLorentzFactor.ndim))
+print("shape:{0}".format(NormalLorentzFactor.shape))
+
+
